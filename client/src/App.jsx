269200becc504b38a38f81ae87579a1030b41b3c
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-10-06 21:43:16 +0530

    mern project backend and frontend part

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
