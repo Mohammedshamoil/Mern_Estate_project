068b0845846fb3f6c65e9c21bdf545af5a0081bb
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-10-07 22:34:08 +0530

    header responsive

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,35 +5,35 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between  items-center max-w-6xl mx-auto p-3">
+      <div className="flex justify-around items-center">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm md:text-xl sm:flex  sm:flex-wrap ">
             <span className="text-slate-500">shammu</span>
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 rounded-lg flex items-center p-3 w-24 sm:w-64">
+        <form className="bg-slate-100 rounded-lg flex p-3 items-center   ">
           <input
             type="text"
             placeholder="Search..."
-            className="bg-transparent focus:outline-none"
+            className="bg-transparent text-lg focus:outline-none"
           />
-          <FaSearch className=" text-slate-500"></FaSearch>
+          <FaSearch className=" text-slate-500 "></FaSearch>
         </form>
-        <ul className="flex gap-4">
+        <ul className="flex md:gap-4">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden  text-lg sm:inline text-slate-700 hover:underline cursor-pointer">
               Home
             </li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline cursor-pointer">
+            <li className="hidden  text-lg sm:inline text-slate-700 hover:underline cursor-pointer">
               About
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className=" text-slate-700 hover:underline cursor-pointer">
-              Sign in
+            <li className="text-lg text-slate-700 hover:underline cursor-pointer">
+              Signin
             </li>
           </Link>
         </ul>
