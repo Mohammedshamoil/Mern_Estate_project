13a68e957b07e6bb27ea7e38be5302db78a8b7b9
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-10-07 20:33:57 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from './routes/User.route.js'
 import dotenv from "dotenv";
 dotenv.config();
+const app = express();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -10,7 +12,9 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-const app = express();
+
 app.listen(3000, (req, res) => {
   console.log("server is running");
 });
+
+app.use("/api/user",userRouter)
