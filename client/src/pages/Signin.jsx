7d5d7ea83e6fd3eef7313a3739f3cb877ca5a090
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-10-10 20:09:04 +0530

    add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  sigInStart,
+  siginFailure,
+  siginSuccess,
+} from "../redux/user/userSlice";
 
 function Signin() {
-
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -17,7 +24,8 @@ function Signin() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      // setLoading(true);
+      dispatch(sigInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -28,61 +36,58 @@ function Signin() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        // setLoading(false);
+        // setError(data.message);
+        dispatch(siginFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      // setLoading(false);
+      // setError(null);
+      dispatch(siginSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      dispatch(siginFailure(error.message));
     }
   };
 
-
   return (
     <div className="p-3 max-w-xl  mx-auto  ">
-    <h1 className="text-3xl text-center font-semibold my-7">
-      Login 
-    </h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Login</h1>
 
-    <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-     
-      <input
-        type="email"
-        placeholder="email"
-        className="border p-3 rounded-lg  "
-        onChange={handleChange}
-        id="email"
-      />
-      <input
-        type="password"
-        placeholder="password"
-        className="border p-3 rounded-lg "
-        onChange={handleChange}
-        id="password"
-      />
-      <button
-        disabled={loading}
-        className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
-      >
-        {loading ? "Loading ..." : "Sign in"}
-      </button>
-    </form>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          placeholder="email"
+          className="border p-3 rounded-lg  "
+          onChange={handleChange}
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border p-3 rounded-lg "
+          onChange={handleChange}
+          id="password"
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading ..." : "Sign in"}
+        </button>
+      </form>
 
-    <div className="flex gap-2 mt-5 ">
-      <p> Dont have an account?</p>
-      <Link to={"/sign-up"}>
-        <span className="text-blue-700 hover:underline">Sign up</span>
-      </Link>
+      <div className="flex gap-2 mt-5 ">
+        <p> Dont have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700 hover:underline">Sign up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
-    {error && <p className="text-red-500 mt-5">{error}</p>}
-  </div>
-);
-    
-  
+  );
 }
 
-export default Signin
+export default Signin;
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  currentUser: null,
+  error: null,
+  loading: false,
+};
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    sigInStart: (state) => {
+      state.loading = true;
+    },
+    siginSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    siginFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+  },
+});
+
+export const { sigInStart, siginFailure, siginSuccess } = userSlice.actions;
+export default userSlice.reducer;
