18befdcb6c89653e06d72ee3ef6a908b58bed3e2
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-10-10 17:59:28 +0530

    complete sign in functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,15 +22,15 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) return next(errorHandler, 404, "User Not found!");
-    const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler, 401, "wrong credentials!");
+    if (!validUser) return next(errorHandler (404,"User Not found!"));
+    const validPassword = bcryptjs.compareSync(password,validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials!"));
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     // not show the user to password we use spread opeartor
     const { password: pass, ...rest } = validUser._doc;
     res
-      .cookie("access_tokem", token, { httpOnly: true })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
