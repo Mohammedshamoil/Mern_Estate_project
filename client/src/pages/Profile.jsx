119441cabb12edcbc05bcce57f11aa40929b928e
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-11-04 16:40:06 +0530

    complete update user functinoality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,14 +7,23 @@ import {
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import { storage  } from "../firebase";
+import { storage } from "../firebase";
+import { useDispatch } from "react-redux";
+import {
+  updateUserStart,
+  updateUserFailure,
+  updateUserSuccess,
+} from "../redux/user/userSlice";
 function Profile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser,loading,error } = useSelector((state) => state.user);
   const fileRef = useRef(null);
   const [file, setfile] = useState(undefined);
   const [fileperc, setFilePerc] = useState(0);
   const [fileuploadError, SetFileuploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updatesuccess,setUpdatedSucess]=useState(false)
+  const dispatch = useDispatch();
+  // console.log(formData);
   // console.log(formData);
   // console.log(fileperc);
   // // console.log(file);
@@ -44,22 +53,49 @@ function Profile() {
         // console.log("upload is" + progress + "% done");
         setFilePerc(Math.round(progress));
       },
-      (error) => {  
+      (error) => {
         SetFileuploadError(true);
-        console.log(error)
+        console.log(error);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => 
-          setFormData({ ...formData, avatar: downloadURL })
+        getDownloadURL(uploadTask.snapshot.ref).then(
+          (downloadURL) => setFormData({ ...formData, avatar: downloadURL })
           // console.log('File available at', downloadURL)
         );
       }
     );
   };
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdatedSucess(true)
+      
+    } catch (error) {
+      dispatch(updateUserFailure(error.message));
+    }
+  };
   return (
     <div className="p-3 max-w-xl mx-auto">
       <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           onChange={(e) => setfile(e.target.files[0])}
           type="file"
@@ -69,54 +105,54 @@ function Profile() {
         />
         <img
           onClick={() => fileRef.current.click()}
-          src={ formData.avatar || currentUser.avatar}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="rounded-full h-28 w-28 object-cover cursor-pointer self-center mt-2"
         />
         <p className="text-sm text-center">
-          {fileuploadError?
-          <span className="text-red-400">
-            error Image Upload
-          </span>:
-          fileperc >0 && fileperc<100 ?
-          <span className="text-slate-700">
-            {`Uploading ${fileperc}`}
-          </span>:
-          fileperc===100?
-          <span className="text-green-700">
-            image uploaded successfully 
-          </span>:
-          ""
-
-          }
+          {fileuploadError ? (
+            <span className="text-red-400">error Image Upload</span>
+          ) : fileperc > 0 && fileperc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${fileperc}`}</span>
+          ) : fileperc === 100 ? (
+            <span className="text-green-700">image uploaded successfully</span>
+          ) : (
+            ""
+          )}
         </p>
         <input
           type="text"
+          defaultValue={currentUser.username}
           id="username"
           aria-describedby="helper-text-explanation"
           className="bg-gray-50 border  border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Username"
+          onChange={handleChange}
         />
 
         <input
           type="email"
+          defaultValue={currentUser.email}
           id="email"
           aria-describedby="helper-text-explanation"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Email"
+          onChange={handleChange}
         />
         <input
           type="password"
           id="password"
+          // defaultValue={currentUser.password}
           aria-describedby="helper-text-explanation"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Password"
+          onChange={handleChange}
         />
-        <button
-          type="button"
+        <button disabled={loading}
+          // type="button"
           className=" uppercase text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-bold rounded-lg text-lg px-5 py-3 text-center mr-2 mb-2"
         >
-          Update
+          {loading ? 'Loading...' : 'Update'}
         </button>
       </form>
       <div className="flex justify-between mt-5 ">
@@ -127,6 +163,8 @@ function Profile() {
           Sign out
         </span>
       </div>
+   <p className="text-red-700 mt-5 font-bold"> {error ? error : '' }</p>
+   <p className="text-green-700 font-bold">{updatesuccess?"User is updated successfully" :'' } </p>
     </div>
   );
 }
