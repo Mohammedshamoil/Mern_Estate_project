563e74a336246e968cae5aa2d01a06295328dd0b
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-11-16 13:23:22 +0530

    add image slider to lisiting page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,19 +8,26 @@ import Header from "./Components/Header";
 import PrivateRoute from "./Components/privateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
-    <Header></Header>
+      <Header></Header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<Signin />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route  element={<PrivateRoute/>}>
-        <Route path="/profile" element={<Profile />}></Route>
-        <Route path="/create-listing" element={<CreateListing/>}></Route>
-        <Route path="/update-listing/:listingId" element={<UpdateListing/>}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/listing/:listingId" element={<Listing />}></Route>
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/create-listing" element={<CreateListing />}></Route>
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          ></Route>
         </Route>
       </Routes>
     </BrowserRouter>
