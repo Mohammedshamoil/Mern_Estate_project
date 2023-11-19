ff9a3bfbdb1507ff55f2c07d5eedf04cb4c834a8
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-11-19 17:13:48 +0530

    listing items changes

--- client/src/Components/Listingitems.jsx
+++ client/src/Components/Listingitems.jsx
@@ -9,10 +9,10 @@ function Listingitems({ listing }) {
         <img
           src={listing.imageUrls[0]}
           alt="listing cover"
-          className="h-[320px] sm:h-[300px] w-full object-cover hover:scale-105 transition-scale duration-300"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
-        <div className="p-3 flex flex-col gap-3 m-4 w-full">
-          <p className="text-xl font-semibold text-slate-700 truncate ">
+        <div className="p-3 flex flex-col gap-2  w-full">
+          <p className="text-lg font-semibold text-slate-700 truncate ">
             {listing.name}
           </p>
           <div className=" flex items-center gap-1">
@@ -23,7 +23,7 @@ function Listingitems({ listing }) {
             </p>
           </div>
           <div className="">
-            <p className="text-md text-gray-600 line-clamp-2">
+            <p className="text-sm text-gray-600 line-clamp-2">
               {listing.description}
             </p>
             <p className="text-slate-600 mt-2 font-semibold ">
