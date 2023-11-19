2a6a805022016fc8c4f0a01a62b317d93fc579fe
mohammedshamoil <mohammedshamoil11@gmail.com>
2023-11-19 15:15:59 +0530

    add pre installination

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -18,22 +19,29 @@ mongoose
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 app.listen(3000, (req, res) => {
   console.log("server is running");
 });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing",listingRouter);
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 //middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server Error";
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
-
-  })
+  });
 });
